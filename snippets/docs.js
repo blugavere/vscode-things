537e21b9aceffe0ef07bba59415b2806decbfc7a
blugavere <b.lugavere@gmail.com>
2017-02-07 22:46:40 -0500

    docs

--- snippets/docs.js
+++ snippets/docs.js
@@ -2,7 +2,7 @@
      * @swagger
      * /firstPartyOrderEntries:
      *   get:
-     *     tags: 
+     *     tags:
      *       - First Party Orders
      *     description: Find all First Party Orders
      *     produces:
@@ -16,52 +16,90 @@
      *              $ref: '#/definitions/FirstPartyOrderEntry'
      */
 
+
+
+
+
+
+
+
 module.exports = {
-    "swagger:findAll": {
-        "prefix": "swagger:post",
-        "body": [
-            "    /**",
-            "     * @swagger",
-            "     * /v1/ROUTE:",
-            "     *   get:",
-            "     *     tags:",
-            "     *       - $1s",
-            "     *     description: Find All $1s",
-            "     *     produces:",
-            "     *       - application/json",
-            "     *     responses:",
-            "     *         200:",
-            "     *             description: New $1",
-            "     *             schema:",
-            "     *                 \\$ref: '#/definitions/$1",
-            "	 */"
+    'docs:findAll': {
+        'prefix': 'docs:findAll',
+        'body': [
+            '   docs.add(\'/$1\', docFactory.get()',
+            '       .description(\'Find All $1s\')',
+            '       .summary(\'Find All\')',
+            '       .setModel(\'$1\')',
+            '       .idParam()',
+            '       .onSuccess(200, {',
+            '           description: \'Response\',',
+            '           schema: Doc.arrayOf(Doc.model(\'$1\'))',
+            '       })',
+            '       .build());'
         ]
     },
-    "swagger:post": {
-        "prefix": "swagger:post",
-        "body": [
-            "    /**",
-            "     * @swagger",
-            "     * /v1/ROUTE:",
-            "     *   post:",
-            "     *     tags:",
-            "     *       - $1s",
-            "     *     description: Create $1",
-            "     *     produces:",
-            "     *       - application/json",
-            "     *     parameters:",
-            "     *       - name: $1",
-            "     *         description: $1 object",
-            "     *         in: body",
-            "     *         required: true",
-            "     *         schema:",
-            "     *           \\$ref: '#/definitions/$1'",
-            "     *     responses:",
-            "     *       201:",
-            "     *         description: New $1",
-            "     *         schema:",
-            "     *           \\$ref: '#/definitions/$1",
-            "	 */"
+    'docs:findOne': {
+        'prefix': 'docs:findOne',
+        'body': [
+            '   docs.add(\'/$1/findOne/{id}\', docFactory.get()',
+            '       .description(\'Find One $1\')',
+            '       .summary(\'Find One\')',
+            '       .setModel(\'$1\')',
+            '       .idParam()',
+            '       .onSuccess(200, {',
+            '           description: \'Response\',',
+            '           schema: Doc.model(\'$1\')',
+            '       })',
+            '       .build());'
+        ]
+    },
+    'swagger:findAll': {
+        'prefix': 'swagger:post',
+        'body': [
+            '    /**',
+            '     * @swagger',
+            '     * /v1/ROUTE:',
+            '     *   get:',
+            '     *     tags:',
+            '     *       - $1s',
+            '     *     description: Find All $1s',
+            '     *     produces:',
+            '     *       - application/json',
+            '     *     responses:',
+            '     *         200:',
+            '     *             description: New $1',
+            '     *             schema:',
+            '     *                 \\$ref: \'#/definitions/$1',
+            '	 */'
+        ]
+    },
+    'swagger:post': {
+        'prefix': 'swagger:post',
+        'body': [
+            '    /**',
+            '     * @swagger',
+            '     * /v1/ROUTE:',
+            '     *   post:',
+            '     *     tags:',
+            '     *       - $1s',
+            '     *     description: Create $1',
+            '     *     produces:',
+            '     *       - application/json',
+            '     *     parameters:',
+            '     *       - name: $1',
+            '     *         description: $1 object',
+            '     *         in: body',
+            '     *         required: true',
+            '     *         schema:',
+            '     *           \\$ref: \'#/definitions/$1\'',
+            '     *     responses:',
+            '     *       201:',
+            '     *         description: New $1',
+            '     *         schema:',
+            '     *           \\$ref: \'#/definitions/$1',
+            '	 */'
         ]
     },
 }
+;
