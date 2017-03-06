e95412148d1ef30f427107a469e6226287900cc9
Ben Lugavere <b.lugavere@gmail.com>
2017-03-06 14:15:25 -0500

    env

--- snippets/Builder.js
+++ snippets/Builder.js
@@ -58,8 +58,8 @@ class Builder {
     this.snippet = this.snippet.concat(
       '/**',
       '* helper: ${TM_FILEPATH}',
-      '* mocha --require clarify ./path/to/file.test.js --watch',
-      '* istanbul cover --print both node_modules/.bin/_mocha -- ./path/to/file.test.js',
+      '* NODE_ENV=test mocha --require clarify ./path/to/file.test.js --watch',
+      '* NODE_ENV=test istanbul cover --print both node_modules/.bin/_mocha -- ./path/to/file.test.js',
       '* eslint ./path/to/file.test.js --watch',
       '*/'
     );
